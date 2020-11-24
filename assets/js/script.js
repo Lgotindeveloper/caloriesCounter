6f5c8d98a9700a753148fcc36d978bdfa981b3e7
lgotindev <lgotindev@gmail.com>
2020-11-25 02:02:43 +0600

    final

---
+++ assets/js/script.js
@@ -0,0 +1,47 @@
+let resultButton = document.querySelector('.result-button');
+let womanGenderButton = document.querySelector('#woman-button');
+let manGenderButton = document.querySelector('#man-button');
+
+let resultWeightMaintenance = document.querySelector('#weight-maintenance');
+let resultWeightLoss = document.querySelector('#weight-loss');
+let resultWeightGain = document.querySelector('#weight-gain');
+
+const ageIndex = 5;
+const weightIndex = 10;
+const heightIndex = 6.25;
+const womanIndex = -165;
+const manIndex = 5;
+
+womanGenderButton.onclick = function() {
+    if (!womanGenderButton.classList.contains('active')) {
+        manGenderButton.classList.remove('active');
+        womanGenderButton.classList.add('active');
+    }
+}
+
+manGenderButton.onclick = function() {
+    if (!manGenderButton.classList.contains('active')) {
+        womanGenderButton.classList.remove('active');
+        manGenderButton.classList.add('active');
+    }
+}
+
+
+resultButton.onclick = function() {
+    //DCI Index = (weight * 10 + height * 6.25 - age * 5 - 165(woman)) * activityIndex
+    let ageInput = document.querySelector('#age-input').value;
+    let heightInput = document.querySelector('#height-input').value;
+    let weightInput = document.querySelector('#weight-input').value;
+    let resultSelectedRadio = document.querySelector('input[name="activity-value"]:checked')
+        .dataset.index;
+    let genderSelectValue = womanGenderButton.classList.contains('active') ? womanIndex 
+        : manIndex; 
+
+    let DCIIndex = (weightInput * weightIndex + heightInput * heightIndex - ageInput *
+         ageIndex + (genderSelectValue)) * resultSelectedRadio;
+
+    resultWeightMaintenance.innerHTML = DCIIndex;
+    resultWeightGain.innerHTML = DCIIndex + 300;
+    resultWeightLoss.innerHTML = DCIIndex - 300;
+
+}
