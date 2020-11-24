04cd5350bed776194b48de5f7cc7f2cf1056eb73
lgotindev <lgotindev@gmail.com>
2020-11-25 02:52:13 +0600

    EVERYTHING SEEMS to be ready

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,22 +12,7 @@ const heightIndex = 6.25;
 const womanIndex = -165;
 const manIndex = 5;
 
-womanGenderButton.onclick = function() {
-    if (!womanGenderButton.classList.contains('active')) {
-        manGenderButton.classList.remove('active');
-        womanGenderButton.classList.add('active');
-    }
-}
-
-manGenderButton.onclick = function() {
-    if (!manGenderButton.classList.contains('active')) {
-        womanGenderButton.classList.remove('active');
-        manGenderButton.classList.add('active');
-    }
-}
-
-
-resultButton.onclick = function() {
+function calculateIndex() {
     //DCI Index = (weight * 10 + height * 6.25 - age * 5 - 165(woman)) * activityIndex
     let ageInput = document.querySelector('#age-input').value;
     let heightInput = document.querySelector('#height-input').value;
@@ -40,8 +25,26 @@ resultButton.onclick = function() {
     let DCIIndex = (weightInput * weightIndex + heightInput * heightIndex - ageInput *
          ageIndex + (genderSelectValue)) * resultSelectedRadio;
 
-    resultWeightMaintenance.innerHTML = DCIIndex;
-    resultWeightGain.innerHTML = DCIIndex + 300;
-    resultWeightLoss.innerHTML = DCIIndex - 300;
+    //delete fractional part
+    DCIIndex = Math.trunc(DCIIndex);
 
+    resultWeightMaintenance.innerHTML = DCIIndex + ' ккал';
+    resultWeightGain.innerHTML = DCIIndex + 300 + ' ккал';
+    resultWeightLoss.innerHTML = DCIIndex - 300 + ' ккал';
+
+    
+}
+
+womanGenderButton.onclick = function() {
+    if (!womanGenderButton.classList.contains('active')) {
+        manGenderButton.classList.remove('active');
+        womanGenderButton.classList.add('active');
+    }
+}
+
+manGenderButton.onclick = function() {
+    if (!manGenderButton.classList.contains('active')) {
+        womanGenderButton.classList.remove('active');
+        manGenderButton.classList.add('active');
+    }
 }
